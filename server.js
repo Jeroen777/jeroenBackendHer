f662c53ce3a5a77412af5a46d938750b117e762c
Jeroen777 <jeroenbesselink98@gmail.com>
2021-03-04 21:09:22 +0100

    keuze maken functie

--- server.js
+++ server.js
@@ -8,4 +8,20 @@ const multer = require('multer');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const port = 8000;
 
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs")
+app.set('views/pages', 'view')
+app.get('/', keuzesMaken);
+app.get('pages/', keuzesMaken);
+
+//formulier keuzes maken
+function keuzesMaken(req, res){ 
+    res.render('pages/keuzes');
+  };
+  
+  app.post('/keuzes', urlencodedParser, function(req, res){ 
+    console.log(req.body);
+    res.render('pages/keuzes-gelukt', {data: req.body});
+  });
+
 app.listen(port, () => console.log(`app running on port: ${port}`));
