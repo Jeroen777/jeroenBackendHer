ac6a79400af65acccff422ab5016a8f5ef8e675f
Jeroen777 <jeroenbesselink98@gmail.com>
2021-03-07 17:32:45 +0100

    beatify de code

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const slug = require("slug");
 const bodyParser = require('body-parser');
 const multer = require('multer');
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const urlencodedParser = bodyParser.urlencoded({
+  extended: false
+});
 const port = 8000;
 
 app.use(express.static(__dirname + '/public'));
@@ -23,20 +25,34 @@ app.get('/informatie', informatieShow)
 //   res.render('pages/informatie', { title: 'ja', blogs });
 // });
 
-app.use(function(req, res) { res.status(404).render('pages/not-found.ejs')});
+app.use(function (req, res) {
+  res.status(404).render('pages/not-found.ejs')
+});
 
-function interestShow(req, res){  
-    res.render('pages/interest', {title: 'ja' });
-  };
+function interestShow(req, res) {
+  res.render('pages/interest', {
+    title: 'ja'
+  });
+};
 
-  function informatieShow(req, res){
-    const interestView = [
-      {leuk: 'Ik weet niet', omschrijving: 'het zal wel'},
-      {leuk: 'tweeee', omschrijving: 'het zal weltweeeee'},
-      {leuk: 'dreieeee', omschrijving: 'het drieeee wel'}
-    ]; 
-    res.render('pages/informatie', {interestView });
-  };
-  
+function informatieShow(req, res) {
+  const interestView = [{
+      leuk: 'Ik weet niet',
+      omschrijving: 'het zal wel'
+    },
+    {
+      leuk: 'tweeee',
+      omschrijving: 'het zal weltweeeee'
+    },
+    {
+      leuk: 'dreieeee',
+      omschrijving: 'het drieeee wel'
+    }
+  ];
+  res.render('pages/informatie', {
+    interestView
+  });
+};
+
 
 app.listen(port, () => console.log(`app running on port: ${port}`));
